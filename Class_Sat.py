0f944852544d7fe0a2c347d84fe540874605eab9
Juan Puerta <jfpuerta@gmail.com>
2025-05-29 12:20:18 -0500

    Add status method of satellite

--- Class_Sat.py
+++ Class_Sat.py
@@ -7,3 +7,6 @@ class Satellite:
         self.orbit_type = orbit_type
         self.status = "Idle"
         self.data_collected = 0 # in GB
+        
+    def __str__(self):
+        return f"Satellite: {self.name} ({self.orbit_type}) at {self.altitude} km. Status: {self.status}. Data: {self.data_collected} GB."
